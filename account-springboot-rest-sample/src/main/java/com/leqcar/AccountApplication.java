6904e528d7379e81c889568f579a2c83fa0cdc1b
leqcar <leqcar@gmail.com>
2016-05-07 23:10:46 +0800

    Initial settings

---
+++ account-springboot-rest-sample/src/main/java/com/leqcar/AccountApplication.java
@@ -0,0 +1,16 @@
+package com.leqcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+
+}
---
+++ account-springboot-rest-sample/src/main/java/com/leqcar/domain/Account.java
@@ -0,0 +1,30 @@
+package com.leqcar.domain;
+
+/**
+ * Created by JONGT on 5/7/2016.
+ */
+
+public class Account {
+
+    private long id;
+    private String name;
+
+    public Account() {
+
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
