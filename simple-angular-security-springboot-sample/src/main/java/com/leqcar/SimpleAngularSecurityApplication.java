9b1224627aea1ab77dc82675762a745cbca33920
leqcar <leqcar@gmail.com>
2016-05-30 23:51:05 +0800

    simple-angular-security-springboot-sample -> Generate angular-bootstrap.js using Wro4j

--- simple-angular-security-springboot-sample/src/main/java/com/leqcar/SimpleAngularSecurityApplication.java
+++ simple-angular-security-springboot-sample/src/main/java/com/leqcar/SimpleAngularSecurityApplication.java
@@ -3,6 +3,11 @@ package com.leqcar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Reference :
+ * https://spring.io/blog/2015/01/12/spring-and-angular-js-a-secure-single-page-application
+ */
+
 @SpringBootApplication
 public class SimpleAngularSecurityApplication {
 
