1990fe2575e6b4b031095d78703fc84486df7fa4
leqcar <leqcar@gmail.com>
2016-05-30 10:01:21 +0800

    securing-web-springboot-simple-sample -> Initial Commit

---
+++ securing-web-springboot-simple-sample/src/main/java/com/leqcar/SecuringWebApplication.java
@@ -0,0 +1,21 @@
+package com.leqcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by jongtenerife on 30/05/2016.
+ */
+@SpringBootApplication
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
+public class SecuringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuringWebApplication.class, args);
+    }
+}
