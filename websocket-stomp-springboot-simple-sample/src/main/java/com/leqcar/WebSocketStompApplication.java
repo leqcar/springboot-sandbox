d058c18888ade3ff0ef561fb43c0c5c9d08381ff
leqcar <leqcar@gmail.com>
2016-05-30 15:02:50 +0800

    websocket-stomp-springboot-simple-sample -> Initial Commit

---
+++ websocket-stomp-springboot-simple-sample/src/main/java/com/leqcar/WebSocketStompApplication.java
@@ -0,0 +1,15 @@
+package com.leqcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jongtenerife on 30/05/2016.
+ */
+@SpringBootApplication
+public class WebSocketStompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketStompApplication.class, args);
+    }
+}
