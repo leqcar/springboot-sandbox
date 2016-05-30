0a61e7df392fe3b8745f753b431a56baf71d150c
leqcar <leqcar@gmail.com>
2016-05-30 15:06:48 +0800

    websocket-stomp-springboot-simple-sample -> Create a resource representation class

---
+++ websocket-stomp-springboot-simple-sample/src/main/java/com/leqcar/hello/Greeting.java
@@ -0,0 +1,17 @@
+package com.leqcar.hello;
+
+/**
+ * Created by jongtenerife on 30/05/2016.
+ */
+public class Greeting {
+
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ websocket-stomp-springboot-simple-sample/src/main/java/com/leqcar/hello/HelloMessage.java
@@ -0,0 +1,16 @@
+package com.leqcar.hello;
+
+/**
+ * Created by jongtenerife on 30/05/2016.
+ */
+public class HelloMessage {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
