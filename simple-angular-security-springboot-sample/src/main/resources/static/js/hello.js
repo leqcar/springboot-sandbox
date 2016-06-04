a6393cf8ff4ae397de80c48b8f4bd7b3d2c4305c
leqcar <leqcar@gmail.com>
2016-06-04 21:36:37 +0800

    simple-angular-security-springboot-sample -> Add Navigation to the Angular Application

--- simple-angular-security-springboot-sample/src/main/resources/static/js/hello.js
+++ simple-angular-security-springboot-sample/src/main/resources/static/js/hello.js
@@ -1,6 +1,25 @@
-angular.module('hello', [])
-  .controller('home', function($scope, $http) {
+angular.module('hello', ['ngRoute'])
+  .config(function($routeProvider, $httpProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl : 'home.html',
+        controller : 'home',
+        controllerAs : 'controller'
+    }).when('/login', {
+        templateUrl : 'login.html',
+        controller : 'navigation',
+        controllerAs : 'controller'
+    }).otherwise('/');
+
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+  })
+
+  .controller('home', function($http) {
+    var self = this;
     $http.get("/resource/").success(function(data) {
-        $scope.greeting = data;
+        self.greeting = data;
     })
+
 })
+  .controller('navigation', function(){});
