2c36ca030cb1d7f42375c2c3d0ff1af79a4e0a8c
leqcar <leqcar@gmail.com>
2016-06-05 12:26:11 +0800

    simple-angular-security-springboot-sample -> Add @RequestMapping '/user' to service the 'authenticate()' function

--- simple-angular-security-springboot-sample/src/main/java/com/leqcar/controller/SimpleController.java
+++ simple-angular-security-springboot-sample/src/main/java/com/leqcar/controller/SimpleController.java
@@ -3,6 +3,7 @@ package com.leqcar.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -18,5 +19,10 @@ public class SimpleController {
         model.put("id", UUID.randomUUID().toString());
         model.put("content", "Hello Angular");
         return model;
+    }
+
+    @RequestMapping("/user")  // to service the 'authenticate()' function
+    public Principal user(Principal user) {
+        return user;
     }
 }
