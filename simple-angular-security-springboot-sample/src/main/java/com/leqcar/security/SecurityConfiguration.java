07c78261f6eb8fd40cb78c561bd37cc4ea60a5c3
leqcar <leqcar@gmail.com>
2016-06-06 15:56:13 +0800

    simple-angular-security-springboot-sample -> Add custom filter CsrfHeaderFilter in SecurityConfiguration

--- simple-angular-security-springboot-sample/src/main/java/com/leqcar/security/SecurityConfiguration.java
+++ simple-angular-security-springboot-sample/src/main/java/com/leqcar/security/SecurityConfiguration.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
@@ -19,6 +22,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
                                 ", /login.html" +
                                 ", /home.html" +
                                 ", /").permitAll()
-                        .anyRequest().authenticated();
+                        .anyRequest().authenticated()
+                        .and()
+                        .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+                        .csrf().csrfTokenRepository(createCsrfTokenRepository());
     }
+
+	private CsrfTokenRepository createCsrfTokenRepository() {
+		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName("X-XRSF-TOKEN");
+		return repository;
+	}
 }
